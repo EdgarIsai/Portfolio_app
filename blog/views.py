84464372a1f2837246d61af24dd58870ac9d2133
Isai <edgarisaiwr@gmail.com>
2019-05-07 21:05:59 -0500

    Add blog templates, now everything is working

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from blog.models import Post, Comment
 from blog.forms import CommentForm
 
+
 def blog_index(request):
     posts = Post.objects.all().order_by('-created_on')
     context = {
