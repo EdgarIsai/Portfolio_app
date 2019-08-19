eb26cab29cddede56d67ab2788279f8fe399567f
Isai <edgarisaiwr@gmail.com>
2019-08-18 20:24:37 -0500

    Add more responsiveness, finished everything up to about me and footer, need to make  desktop version

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = '/home/EdgarIsai/Portfolio_app/personal_portfolio/static'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'personal_portfolio/media')
--- projects/models.py
+++ projects/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = models.FilePathField(path='/img')
+    technology = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="project_image",
+                              blank=False,
+                              default="../static/img/square-ppic.jpg")
