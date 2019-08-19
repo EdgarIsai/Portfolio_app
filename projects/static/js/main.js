e3cdcf6566797e8b039b939779d2180674787f4a
Isai <edgarisaiwr@gmail.com>
2019-08-19 14:49:25 -0500

    Add working sidebar, added technlogies, fixed some bugs and added fonts.

---
+++ projects/static/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    var navIcon = $("#NavbarIcon");
+    var sideBar = $("#side-bar")
+
+    navIcon.click(function () {
+        sideBar.toggleClass("openNavbar");
+    });
+
+    $(document).mouseup(function (e) {
+        if ( $(e.target).closest(navIcon).length === 0 ) {
+            sideBar.removeClass("openNavbar");
+        }
+    });
+});
